01e077e7b0e34f7ca7bba6dffcb6098a95991013
bind <douglasjbinder@gmail.com>
2020-10-25 08:04:22 -0500

    better base

---
+++ src/utils/getAllPostPreviews.js
@@ -0,0 +1,20 @@
+// Flagrantly taken from here https://github.com/tailwindlabs/blog.tailwindcss.com/blob/master/src/getAllPostPreviews.js
+
+function importAll(r) {
+  return r.keys().map((fileName) => ({
+    link: fileName.substr(1).replace(/\/index\.mdx$/, ""),
+    module: r(fileName),
+  }));
+}
+
+function dateSortDesc(a, b) {
+  if (a > b) return -1;
+  if (a < b) return 1;
+  return 0;
+}
+
+export default function getAllPostPreviews() {
+  return importAll(
+    require.context("../../pages/?preview", true, /\.mdx$/)
+  ).sort((a, b) => dateSortDesc(a.module.meta.date, b.module.meta.date));
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,42 @@ module.exports = {
   },
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    mode: "all",
     enabled: process.env.NODE_ENV === "production",
-    content: ["pages/**/*.mdx", "src/**/*.js"],
+    content: ["pages/**/*.mdx", "src/**/*.js", './next.config.js'],
+    options: {
+      extractors: [
+        {
+          extensions: ['mdx'],
+          extractor: (content) => {
+            content = mdx.sync(content)
+
+            // Capture as liberally as possible, including things like `h-(screen-1.5)`
+            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+            // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+            const innerMatches =
+              content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
+
+            return broadMatches.concat(innerMatches)
+          },
+        },
+      ],
+    },
   },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        code: {
+          green: '#b5f4a5',
+          yellow: '#ffe484',
+          purple: '#d9a9ff',
+          red: '#ff8383',
+          blue: '#93ddfd',
+          white: '#fff',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
