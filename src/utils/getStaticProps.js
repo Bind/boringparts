b72265aa05ab7a95c9488355acc4fa7984e4ff24
bind <douglasjbinder@gmail.com>
2020-11-16 11:32:50 -0600

    spelling fix, filter unpublished articles

--- src/utils/getStaticProps.js
+++ src/utils/getStaticProps.js
@@ -3,10 +3,12 @@ import getAllPostPreviews from "./getAllPostPreviews";
 export async function getStaticProps() {
   return {
     props: {
-      posts: getAllPostPreviews().map((post) => ({
-        title: post.module.meta.title,
-        link: post.link,
-      })),
+      posts: getAllPostPreviews()
+        .filter((post) => post.module.meta.published)
+        .map((post) => ({
+          title: post.module.meta.title,
+          link: post.link,
+        })),
     },
   };
 }
